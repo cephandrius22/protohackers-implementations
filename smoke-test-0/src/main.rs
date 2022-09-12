9e0ab3daff07667f0a190a6076219749b833be70
Joachim Wright <joewright1610@gmail.com>
2022-09-12 17:44:30 -0400

    Accept tcp connections.

--- smoke-test-0/src/main.rs
+++ smoke-test-0/src/main.rs
@@ -1,23 +1,31 @@
-use tokio::net::TcpListener;
+use tokio::{net::{TcpListener, TcpStream}, io::AsyncReadExt};
 
 use std::io;
 
+async fn process_socket(mut socket: TcpStream) {
+    println!("got one");
+    let mut buf = vec![0; 1024];
 
-async fn process_socket<T>(socket: T) {
-    println!("got one")
+    // TODO: use a buffered reader here?
+    let n = socket
+        .read(&mut buf)
+        .await
+        .expect("Failed to read data from socket.");
+    
+    println!("{}", String::from_utf8(buf).unwrap());
 }
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     println!("Hello, world!");
 
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    let listener = TcpListener::bind("127.0.0.1:38888").await?;
 
     loop {
         let (socket, _) = listener.accept().await?;
 
-        process_socket(socket).await;
+        tokio::spawn(async move {
+            process_socket(socket).await;
+        });
     }
-
-    Ok(())
 }
